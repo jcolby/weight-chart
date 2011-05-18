33c3e18aa8d94d50adda0779e42e5012f1fa5d19
James Colby <jcolby@gmail.com>
2011-05-17 23:19:21 -0400

    Add license info, comment/clean up code a bit

---
+++ html/update.php
@@ -0,0 +1,50 @@
+<?php
+
+/*************************************************************************
+ *                                                                       *
+ *  Copyright (C) 2011 by James Colby <jcolby@gmail.com>                 *
+ *                                                                       *
+ *  This program is free software: you can redistribute it and/or modify *
+ *  it under the terms of the GNU General Public License as published by *
+ *  the Free Software Foundation, either version 3 of the License, or    *
+ *  at your option) any later version.                                   *
+ *                                                                       *
+ *  This program is distributed in the hope that it will be useful,      *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *  GNU General Public License for more details.                         *
+ *                                                                       *
+ *  You should have received a copy of the GNU General Public License    *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ *                                                                       *
+ *************************************************************************/
+
+// declare local variables
+ $user="weight";
+ $password="pass";
+ $database="weight_chart";
+
+// variables passed in from index.html
+ $date = $_POST['date'];
+ $weight = $_POST['weight'];
+
+// open connection to database
+ $link = mysql_connect("localhost",$user,$password);
+ if (!$link) {
+   die('Could not connect: ' . mysql_error());
+ }
+
+$db_selected = mysql_select_db($database, $link);
+ if (!$db_selected) {
+   die ('Can\'t use database : ' . mysql_error());
+  }
+
+// insert data into database
+ $query = "INSERT INTO daily_weight VALUE ('','$date',$weight)";
+ mysql_query($query);
+
+// close connection to database
+ mysql_close ($link);
+
+?>
+
